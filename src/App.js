56de1a10070999c052181b9165564134a3499b59
mblaul <mattblaul@gmail.com>
2018-05-26 12:26:20 -0400

    Commit to remove node_modules and some other junk.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      projects : []
+    }
+  }
+
+
+  componentWillMount(){
+    this.setState({
+      projects: [
+        {   
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Shopping',
+          category: 'No Fun'
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        <Projects projects = {this.state.projects} />
       </div>
     );
   }
